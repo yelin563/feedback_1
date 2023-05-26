6dabf523cc5c53c98a69aff253505b153c9b65d4
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-05-26 17:51:59 +0900

    Update sample_answer.py

--- sample_answer.py
+++ sample_answer.py
@@ -3,5 +3,5 @@ lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -1
 \\newline\\newline\\newline  \square = -12 \\times 18 \\times \\frac{1}{8} \\times x^{3} \\times x^{3} \\times \\frac{1}{x^{2}} \\times y^{2} \\times y^{3} \\times \\frac{1}{y^{3}} \
 \\newline\\newline\\newline \square = - 27x^{4}y^{2}','(-12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3}\
 \\newline\\newline\\newline -\\frac{12x^{3}y^{2}}{\square} = \\frac{8x^{2}y^{3}}{18x^{3}y^{3}}=\\frac{8}{18} \\times x^{2} \\times \\frac{1}{x^{3}} \\times \\frac{y^{3}}{y^{3}}  \
-\vspace{2\baselineskip} -\\frac{12x^{3}y^{2}}{\square} =\\frac{4}{9x} \
+\\vspace{2\\baselineskip} -\\frac{12x^{3}y^{2}}{\square} =\\frac{4}{9x} \
 \\newline\\newline\\newline \square = (- 12x^{3}y^{2}) \div \\frac{4}{9x} = (- 12x^{3}y^{2}) \\times \\frac{9x}{4} = - 27x^{4}y^{2} ','- 12x^{3}y^{2}','- 12x^{3}y^{2}']
