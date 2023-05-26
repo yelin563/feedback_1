50624831c6f7918e384d4a9b35e1f67a7152135d
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-05-26 17:33:00 +0900

    Update sample_answer.py

--- sample_answer.py
+++ sample_answer.py
@@ -1,1 +1,1 @@
-lst=['0','(- 12x^{3}y^{2}) \div \square \times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -12x^{3}y^{2} \times \frac{1}{A} \times 18x^{3}y^{3} = 8x^{2}y^{3} \\ A = -12x^{3}y^{2} \times 18x^{3}y^{3} \times \frac{1}{8x^{2}y^{3}} \\  A = -12 \times 18 \times \frac{1}{8} \times x^{3} \times x^{3} \times \frac{1}{x^{2}} \times y^{2} \times y^{3} \times \frac{1}{y^{3}}  A = - 27x^{4}y^{2}','- 12x^{3}y^{2}','- 12x^{3}y^{2}','- 12x^{3}y^{2}']
+lst=['0','(- 12x^{3}y^{2}) \div \square \times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -12x^{3}y^{2}','- 12x^{3}y^{2}','- 12x^{3}y^{2}','- 12x^{3}y^{2}']
