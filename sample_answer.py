472afcf0bf746c44443901c0bded4e39df55bc7c
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-05-26 18:10:10 +0900

    Update sample_answer.py

--- sample_answer.py
+++ sample_answer.py
@@ -1,4 +1,5 @@
-lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -12x^{3}y^{2} \\times \\frac{1}{\square} \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
+lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
+\\newline\\,\\newline -12x^{3}y^{2} \\times \\frac{1}{\square} \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
 \\newline\\,\\newline \square = -12x^{3}y^{2} \\times 18x^{3}y^{3} \\times \\frac{1}{8x^{2}y^{3}} \
 \\newline\\,\\newline  \square = -12 \\times 18 \\times \\frac{1}{8} \\times x^{3} \\times x^{3} \\times \\frac{1}{x^{2}} \\times y^{2} \\times y^{3} \\times \\frac{1}{y^{3}} \
 \\newline\\,\\newline \square = - 27x^{4}y^{2}','(-12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3}\
@@ -10,4 +11,9 @@ lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -1
 \\newline\\,\\newline -12 \\times 18  \\times x^{3}  \\times x^{3}  \\times y^{2}  \\times y^{3}  \div \square = 8x^{2}y^{3} \
 \\newline\\,\\newline - 216x^{6}y^{5} \div \square = 8x^{2}y^{3} \
 \\newline\\,\\newline \square = - 216x^{6}y^{5} \div  8x^{2}y^{3} \
-\\newline\\,\\newline \square =  -216 \div  8 \\times x^{6} \div x^{2} \\times y^{5} \div y^{3} = - 27x^{4}y^{2}','- 12x^{3}y^{2}']
+\\newline\\,\\newline \square =  -216 \div  8 \\times x^{6} \div x^{2} \\times y^{5} \div y^{3} = - 27x^{4}y^{2}','\
+( - 12x^{3}y^{2} ) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \square =kx^{a}y^{b}라 하자 \
+\\newline\\,\\newline 계수 k는  -12 \\times 18 /div k = -8 이므로 k= -27 \
+\\newline\\,\\newline x 차수는 x^{3} \\times x^{3} / x^{a} = x^{2} 이므로  x^{a}=x^{4} \
+\\newline\\,\\newline y 차수는 y^{2} \\times y^{3} / y^{b} = y^{3} 이므로  y^{b} = y^{2} \
+\\newline\\,\\newline \square = - 27x^{4}y^{2}']
