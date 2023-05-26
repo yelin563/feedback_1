5ca1c5f3c6ed79fb205ed2479077930dbe6c7a36
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-05-26 18:06:51 +0900

    Update sample_answer.py

--- sample_answer.py
+++ sample_answer.py
@@ -7,7 +7,7 @@ lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \\ -1
 \\newline\\,\\newline \square = (- 12x^{3}y^{2}) \div \\frac{4}{9x} = (- 12x^{3}y^{2}) \\times \\frac{9x}{4} = - 27x^{4}y^{2} ',' \
 (- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
 \\newline\\,\\newline (- 12x^{3}y^{2}) \\times 18x^{3}y^{3} \div \square = 8x^{2}y^{3} \
-\\newline\\,\\newline -12 \\times 18  \\times x^{3}  \\times x^{3}  \\times y^{2}  \\times y^{3}  \div \square = 8x^{2}y^{3} \ 
+\\newline\\,\\newline -12 \\times 18  \\times x^{3}  \\times x^{3}  \\times y^{2}  \\times y^{3}  \div \square = 8x^{2}y^{3} \
 \\newline\\,\\newline - 216x^{6}y^{5} \div \square = 8x^{2}y^{3} \
 \\newline\\,\\newline \square = - 216x^{6}y^{5} \div  8x^{2}y^{3} \
 \\newline\\,\\newline \square =  -216 \div  8 \\times x^{6} \div x^{2} \\times y^{5} \div y^{3} = - 27x^{4}y^{2}','- 12x^{3}y^{2}']
