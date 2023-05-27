082cf445d502980cc29c57ecd8d72bdb24d03e6a
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-05-27 20:31:28 +0900

    Update sample_answer.py

--- sample_answer.py
+++ sample_answer.py
@@ -1,19 +1,19 @@
-lst=['0','(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
-\\newline\\,\\newline -12x^{3}y^{2} \\times \\frac{1}{\square} \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
-\\newline\\,\\newline \square = -12x^{3}y^{2} \\times 18x^{3}y^{3} \\times \\frac{1}{8x^{2}y^{3}} \
-\\newline\\,\\newline  \square = -12 \\times 18 \\times \\frac{1}{8} \\times x^{3} \\times x^{3} \\times \\frac{1}{x^{2}} \\times y^{2} \\times y^{3} \\times \\frac{1}{y^{3}} \
-\\newline\\,\\newline \square = - 27x^{4}y^{2}','(-12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3}\
-\\newline\\,\\newline -\\frac{12x^{3}y^{2}}{\square} = \\frac{8x^{2}y^{3}}{18x^{3}y^{3}}=\\frac{8}{18} \\times x^{2} \\times \\frac{1}{x^{3}} \\times \\frac{y^{3}}{y^{3}}  \
-\\newline\\,\\newline -\\frac{12x^{3}y^{2}}{\square} =\\frac{4}{9x} \
-\\newline\\,\\newline \square = (- 12x^{3}y^{2}) \div \\frac{4}{9x} = (- 12x^{3}y^{2}) \\times \\frac{9x}{4} = - 27x^{4}y^{2} ',' \
-(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \
-\\newline\\,\\newline (- 12x^{3}y^{2}) \\times 18x^{3}y^{3} \div \square = 8x^{2}y^{3} \
-\\newline\\,\\newline -12 \\times 18  \\times x^{3}  \\times x^{3}  \\times y^{2}  \\times y^{3}  \div \square = 8x^{2}y^{3} \
-\\newline\\,\\newline - 216x^{6}y^{5} \div \square = 8x^{2}y^{3} \
-\\newline\\,\\newline \square = - 216x^{6}y^{5} \div  8x^{2}y^{3} \
-\\newline\\,\\newline \square =  -216 \div  8 \\times x^{6} \div x^{2} \\times y^{5} \div y^{3} = - 27x^{4}y^{2}','\
-( - 12x^{3}y^{2} ) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \square =kx^{a}y^{b}라 \; 하자 \
-\\newline\\,\\newline 계수 k는 \;  -12 \\times 18 \div k = -8 이므로 \; k= -27 \
-\\newline\\,\\newline x 차수는 \; x^{3} \\times x^{3} \div x^{a} = x^{2} 이므로 \;  x^{a}=x^{4} \
-\\newline\\,\\newline y 차수는 \; y^{2} \\times y^{3} \div y^{b} = y^{3} 이므로 \; y^{b} = y^{2} \
-\\newline\\,\\newline \square = - 27x^{4}y^{2}']
+lst1=['(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3}', ' -12x^{3}y^{2} \\times \\frac{1}{\square} \\times 18x^{3}y^{3} = 8x^{2}y^{3}',\
+' \square = -12x^{3}y^{2} \\times 18x^{3}y^{3} \\times \\frac{1}{8x^{2}y^{3}}', \
+' \square = -12 \\times 18 \\times \\frac{1}{8} \\times x^{3} \\times x^{3} \\times \\frac{1}{x^{2}} \\times y^{2} \\times y^{3} \\times \\frac{1}{y^{3}}', \
+' \square = - 27x^{4}y^{2}']
+lst2= ['(-12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3}',\
+' -\\frac{12x^{3}y^{2}}{\square} = \\frac{8x^{2}y^{3}}{18x^{3}y^{3}}=\\frac{8}{18} \\times x^{2} \\times \\frac{1}{x^{3}} \\times \\frac{y^{3}}{y^{3}}',  \
+' -\\frac{12x^{3}y^{2}}{\square} =\\frac{4}{9x}', \
+' \square = (- 12x^{3}y^{2}) \div \\frac{4}{9x} = (- 12x^{3}y^{2}) \\times \\frac{9x}{4} = - 27x^{4}y^{2} ']
+lst3=['(- 12x^{3}y^{2}) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3}', \
+'(- 12x^{3}y^{2}) \\times 18x^{3}y^{3} \div \square = 8x^{2}y^{3}', \
+' -12 \\times 18  \\times x^{3}  \\times x^{3}  \\times y^{2}  \\times y^{3}  \div \square = 8x^{2}y^{3}', \
+' - 216x^{6}y^{5} \div \square = 8x^{2}y^{3}',\
+' \square = - 216x^{6}y^{5} \div  8x^{2}y^{3} ',\
+' \square =  -216 \div  8 \\times x^{6} \div x^{2} \\times y^{5} \div y^{3} = - 27x^{4}y^{2}']
+lst4=['( - 12x^{3}y^{2} ) \div \square \\times 18x^{3}y^{3} = 8x^{2}y^{3} \square =kx^{a}y^{b}라 \; 하자', \
+'계수 k는 \;  -12 \\times 18 \div k = -8 이므로 \; k= -27', \
+' x 차수는 \; x^{3} \\times x^{3} \div x^{a} = x^{2} 이므로 \;  x^{a}=x^{4}', \
+' y 차수는 \; y^{2} \\times y^{3} \div y^{b} = y^{3} 이므로 \; y^{b} = y^{2}', \
+' \square = - 27x^{4}y^{2}']
