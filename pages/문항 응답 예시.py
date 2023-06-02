71ba9f078a6d5854de7f2a3319fa6210dbd7445c
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-06-02 19:12:45 +0900

    Add files via upload

---
+++ pages/문항 응답 예시.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("문항 루브릭")
+st.divider()
+st.header("Part 1 거듭제곱")
+st.image('images/part1_rubric.png', use_column_width=True, caption = "part1_rubric")
+st.header("Part 2 단항식의 곱셈과 나눗셈, 다항식의 덧셈과 뺄셈")
+st.image('images/part2_rubric.png', use_column_width=True, caption = "part2_rubric")
+st.header("Part 3 단항식과 다항식의 곱셈 나눗셈")
+st.image('images/part3_rubric.png', use_column_width=True, caption = "part3_rubric")
