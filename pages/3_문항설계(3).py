ab22badf08d0b1efec69c96bfa0f06d0b6a5ebed
yelin563 <126746403+yelin563@users.noreply.github.com>
2023-06-05 18:24:29 +0900

    Update 3_문항설계(3).py

--- pages/3_문항설계(3).py
+++ pages/3_문항설계(3).py
@@ -100,8 +100,9 @@ pad_ten = torch.tensor(pad_all)
 pad_ten = pad_ten.reshape(1,max_len)
 y = model_all(pad_ten)
 label_all = y.squeeze().detach().cpu().numpy().round()
-print("label_all :",label)
-if label_all[0]==1:
+print("label_all :",label_all)
+if len(label_all)>0:
     st.write('정답입니다!')
+    
 else:
     st.write('오답입니다ㅜㅜ')
